dbbc852435cf7eb3343019d56125e3be4a6a1cec
DrawnToStrife <32200041+DrawnToStrife@users.noreply.github.com>
2017-10-02 13:34:05 -0400

    Add files via upload

---
+++ Player/PlayerCameraY.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraY : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        float mouseVertical = -(Input.GetAxis("Mouse Y"));
+        Vector3 lookVert = new Vector3(mouseVertical, 0);
+        transform.Rotate(lookVert);
+    }
+}
