d80abe7688e3bfd2024553bbb066255931cace70
BACompton <compton.boyd@gmail.com>
2017-11-10 01:04:37 -0500

    Added Wires and seprated filter and transmit modes

--- Assets/Scripts/Puzzle/PuzzleDevice.cs
+++ Assets/Scripts/Puzzle/PuzzleDevice.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 namespace Puzzle {
     /// <summary> Detatils how a device should translate input signals to output signals </summary>
-    public enum DeviceBehavior { AND, OR }
+    public enum DeviceBehavior { STATIC, SINGLE, AND, OR }
 
     /// <summary> Details the behaviors of a puzzle device. </summary>
     public class PuzzleDevice : MonoBehaviour {
@@ -14,6 +14,8 @@ namespace Puzzle {
         public bool active = false;
         /// <summary> Is the device transmitting </summary>
         public bool transmit = false;
+        /// <summary> The color of the transmition </summary>
+        public Color trasmitColor = new Color(1, 1, 1);
         /// <summary> Flags the device evaluate the inputs </summary>
         public bool update = false;
         /// <summary> A list of all the device's inputs. </summary>
@@ -21,7 +23,11 @@ namespace Puzzle {
         /// <summary> A list of all the device's outputs. </summary>
         public List<PuzzleDevice> outputs;
         /// <summary> Details how the device should translate inputs to outputs </summary>
-        public DeviceBehavior mode = DeviceBehavior.AND;
+        public DeviceBehavior transmitMode = DeviceBehavior.STATIC;
+        /// <summary> Details how the device should translate input colors to output colors </summary>
+        public DeviceBehavior filterMode = DeviceBehavior.STATIC;
+
+        private bool queueUpdate = false;
 
         // -------------------------- Unity Functions --------------------------
 
@@ -33,15 +39,23 @@ namespace Puzzle {
         }
 
         void Update() {
+            if(queueUpdate) {
+                queueUpdate = false;
+                update = false;
+            }
+
             if(update) {
                 bool prevXmit = transmit;
                 transmit = IsEnabled() && active;
-                
+
+                if (transmit)
+                    trasmitColor = FilterColor();
+
                 // Signal outputs to update
                 if(prevXmit != transmit)
                     foreach (PuzzleDevice device in outputs)
-                        device.update = true; // TODO: Make some way to stop cycle
-                update = false;
+                        device.update = true;
+                queueUpdate = true;
             }
         }
 
@@ -50,7 +64,13 @@ namespace Puzzle {
         /// <summary> Determines if the device is enabled according to its inputs and the devices behavior mode. </summary>
         /// <returns> True if the device is enabled; Otherwise false.</returns>
         private bool IsEnabled() {
-            switch (mode) {
+            switch (transmitMode) {
+                case DeviceBehavior.STATIC:
+                    return true;
+                case DeviceBehavior.SINGLE:
+                    if (inputs.Count > 0)
+                        return inputs[0].transmit;
+                    return false;
                 case DeviceBehavior.AND:
                     foreach (PuzzleDevice input in inputs)
                         if (!input.transmit)
@@ -63,6 +83,40 @@ namespace Puzzle {
                     return false;
                 default: return false;
             }
+        }
+
+        /// <summary> Determines the filtered transmit color. </summary>
+        /// <returns> The filtered color </returns>
+        private Color FilterColor() {
+            Color c = new Color(0, 0, 0);
+
+            switch (filterMode) {
+                case DeviceBehavior.STATIC:
+                    c = trasmitColor; break;
+                case DeviceBehavior.SINGLE:
+                    if (inputs.Count > 0 && inputs[0].transmit)
+                        c = inputs[0].trasmitColor;
+                    break;
+                case DeviceBehavior.AND:
+                    c = new Color(1, 1, 1);
+
+                    foreach (PuzzleDevice input in inputs)
+                        if (input.transmit) {
+                            c.r = ((int)(c.r * 255) & (int)(input.trasmitColor.r * 255)) / 255;
+                            c.g = ((int)(c.g * 255) & (int)(input.trasmitColor.g * 255)) / 255;
+                            c.b = ((int)(c.b * 255) & (int)(input.trasmitColor.b * 255)) / 255;
+                        }
+                    break;
+                case DeviceBehavior.OR:
+                    foreach (PuzzleDevice input in inputs)
+                        if (input.transmit) {
+                            c.r = ((int)(c.r * 255) | (int)(input.trasmitColor.r * 255)) / 255;
+                            c.g = ((int)(c.g * 255) | (int)(input.trasmitColor.g * 255)) / 255;
+                            c.b = ((int)(c.b * 255) | (int)(input.trasmitColor.b * 255)) / 255;
+                        }
+                    break;
+            }
+            return c;
         }
     }
 }
---
+++ Assets/Scripts/Puzzle/Wire.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Puzzle {
+    public class Wire : MonoBehaviour {
+        // -------------------------- Unity Script Variables --------------------------
+
+        /// <summary> The color the wires appears as when off. </summary>
+        public Color off = new Color(0, 0, 0);
+        /// <summary> The amount of time before the wire is fully active </summary>
+        public float delay = 0; // TODO: Translate visually to Device
+
+        // -------------------------- Class Variables --------------------------
+
+        /// <summary> The button's input component. </summary>
+        private PuzzleDevice device;
+        /// <summary> Objects primary render material </summary>
+        private Material mat;
+        private float left;
+        /// <summary> The target color </summary>
+        private Color target, curr;
+        /// <summary> The transition color </summary>
+        private float r, g, b;
+
+        // -------------------------- Unity Script Functions -------------------------- 
+
+        // Use this for initialization
+        void Start() {
+            device = GetComponent<PuzzleDevice>();
+            mat = GetComponent<Renderer>().material;
+
+            left = delay;
+            mat.color = off;
+            device.trasmitColor = off;
+            target = off;
+        }
+
+        // Update is called once per frame
+        void Update() {
+            if(device.update) {
+                target = off;
+
+                if (device.transmit)
+                    target = device.trasmitColor;
+                
+                r = (target.r - mat.color.r) / delay;
+                g = (target.g - mat.color.g) / delay;
+                b = (target.b - mat.color.b) / delay;
+
+                left = 0;
+                curr = mat.color;
+            }
+
+            if (left < delay) {
+                curr.r += (r * Time.deltaTime);
+                curr.g += (g * Time.deltaTime);
+                curr.b += (b * Time.deltaTime);
+
+                mat.color = curr;
+                left += Time.deltaTime;
+            }  else
+                mat.color = target;
+            device.trasmitColor = mat.color;
+        }
+    }
+}
